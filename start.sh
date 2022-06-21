a49f27c174e2c8efb719a5891dc0c5aeda5dbaae
Donovan Tarsis <donovan.tarsis@ifood.com.br>
2022-06-21 20:04:21 +0000

    Revert "Merge branch 'feature-implment-test' into 'main'"
    This reverts merge request !2

--- start.sh
+++
@@ -1,6 +0,0 @@
-mvn clean compile package -Dmaven.test.skip -DskipTests -Dmaven.javadoc.skip=true
-
-export CLASSPATH=target/api.order.consomer.sqs-1.0-SNAPSHOT.jar
-export className=Application
-echo "## Running $className..."
-mvn exec:java -Dexec.mainClass="br.com.api.order.consomer.sqs.$className"
